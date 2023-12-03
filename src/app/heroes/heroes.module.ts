f05ade03d58400faae77f58fb2efb8daeab5aa54
david <davidchilatra@gmail.com>
2023-12-03 18:46:52 -0500

    Se obtienen heroes con url dedes pipe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { CardComponent } from './components/card/card.component';
     NewPageComponent,
     SearchPageComponent,
     LayoutPageComponent,
-    CardComponent
+    CardComponent,
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,
